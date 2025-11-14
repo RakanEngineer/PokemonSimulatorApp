4d2ce1019e1e6cd0c20a4eede2fa3a4e448e9f79
RakanEngineer <rakan.engineer@gmail.com>
2025-11-14 09:57:58 +0100

    Add attacks (2 per ElementType):

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@
         {
             Console.WriteLine("Welcome to the Pokemon Simulator!");
             Console.WriteLine("---------------------------------");
-            var flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
+
+            Attack flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
+            Attack ember = new Attack("Ember", ElementType.Fire, 6);
+
+            Attack waterGun = new Attack("Water Gun", ElementType.Water, 10);
+            Attack hydroPump = new Attack("Hydro Pump", ElementType.Water, 5);
+
+            Attack vineWhip = new Attack("Vine Whip", ElementType.Grass, 8);
+            Attack solarBeam = new Attack("Solar Beam", ElementType.Grass, 4);
+
             flamethrower.Use(1);
         }
     }
